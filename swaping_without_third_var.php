bb116ad5b1a90f3a0a710bd7ab1827e711397865
MuhammadIbrahim28 <mibrahim17sw28@gmail.com>
2025-09-07 06:27:01 -0700

    Swapping without third variable

---
+++ swaping_without_third_var.php
@@ -0,0 +1,11 @@
+<?php
+$a=10;
+$b=20;
+echo "Before Value of a :".$a." Value of b :".$b;
+echo "<br/>";
+$a = $a+$b;
+$b = $a-$b;
+$a= $a-$b;
+echo "After Value of a :".$a." Value of b :".$b;
+
+?>
